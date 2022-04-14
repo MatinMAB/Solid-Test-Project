8cc082b861d61b0c9f829f9f5979424e26148bab
Matin_MAB <matinmab789@gmail.com>
2022-04-14 17:38:18 +0430

    make register-form and style it

--- src/components/Register/RegisterForm.js
+++ src/components/Register/RegisterForm.js
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
 
+//Import Axios
+import axios from "axios";
+
 //Import Link Router
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 //Import Styles
 import styles from "./RegisterForm.module.css";
@@ -9,15 +12,78 @@ import styles from "./RegisterForm.module.css";
 //Material UI
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
-import { Typography, Grid, Button } from "@mui/material";
+import {
+  Typography,
+  Grid,
+  Button,
+  InputAdornment,
+  IconButton,
+} from "@mui/material";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
 
+//Register Functional Component
 const Register = () => {
+  const navigate = useNavigate();
+
+  //States
+  const [showPassword, setShowPassword] = useState(false);
+  const [loading, setLoading] = useState(false);
   const [user, setUser] = useState({
     firstname: "",
     lastname: "",
     phone: "",
     password: "",
   });
+
+  //Functions
+  const firstnameHandler = (event) => {
+    setUser((prevUser) => ({
+      ...prevUser,
+      firstname: event.target.value,
+    }));
+  };
+  const lastnameHandler = (event) => {
+    setUser((prevUser) => ({
+      ...prevUser,
+      lastname: event.target.value,
+    }));
+  };
+  const phoneHandler = (event) => {
+    setUser((prevUser) => ({
+      ...prevUser,
+      phone: event.target.value,
+    }));
+  };
+  const passwordHandler = (event) => {
+    setUser((prevUser) => ({
+      ...prevUser,
+      password: event.target.value,
+    }));
+  };
+  const showPasswordHandler = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const signUp = () => {
+    setLoading(true);
+    axios
+      .post(
+        `http://chl-api.rahkardigital.com/API/V1/User/register?password=${user.password}&firstname=${user.firstname}&lastname=${user.lastname}&phone=${user.phone}`
+      )
+      .then((response) => {
+        console.log(response.data);
+        if (response.data.ok) {
+          navigate("/");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
 
   return (
     <>
@@ -35,12 +101,13 @@ const Register = () => {
           >
             فرم ثبت نام
           </Typography>
-          <Grid container spacing={3} xs={{ width: "600px" }}>
+          <Grid container spacing={3}>
             <Grid item xs={12} sm={6}>
               <TextField
                 id="outlined-error-helper-text"
                 label="نام *"
                 value={user.firstname}
+                onChange={firstnameHandler}
                 sx={{ width: "100%" }}
               />
             </Grid>
@@ -49,6 +116,7 @@ const Register = () => {
                 id="outlined-error-helper-text"
                 label="نام خانوادگی *"
                 value={user.lastname}
+                onChange={lastnameHandler}
                 sx={{ width: "100%" }}
               />
             </Grid>
@@ -57,24 +125,42 @@ const Register = () => {
                 id="outlined-error-helper-text"
                 label="شماره همراه *"
                 value={user.phone}
+                onChange={phoneHandler}
                 sx={{ width: "100%" }}
               />
             </Grid>
             <Grid item xs={12} sm={6}>
               <TextField
                 id="outlined-error-helper-text"
+                type={showPassword ? "text" : "password"}
                 label="رمز عبور *"
                 value={user.password}
+                onChange={passwordHandler}
                 sx={{ width: "100%" }}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={showPasswordHandler}
+                        edge="end"
+                      >
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>{" "}
+                    </InputAdornment>
+                  ),
+                }}
               />
             </Grid>
             <Grid item xs={12}>
               <Button
+                onClick={signUp}
+                disabled={loading}
                 variant="contained"
                 color="primary"
                 sx={{ width: "100%" }}
               >
-                ثبت نام
+                {loading ? <span>صبر کنید ...</span> : <span>ثبت نام</span>}
               </Button>
             </Grid>
           </Grid>
